9371709342951d579ae5e02c6532ca26c5c2f6f0
Diego Urrutia Astorga <mika.cl@gmail.com>
2018-11-09 10:24:23 -0300

    Added: Initial version.

---
+++ app/src/androidTest/java/cl/ucn/disc/dsm/base/ExampleInstrumentedTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2018.  Diego Urrutia Astorga <durrutia@ucn.cl>
+ * This work is licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.
+ * http://creativecommons.org/licenses/by-nc/4.0/
+ *
+ */
+
+package cl.ucn.disc.dsm.base;
+
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("cl.ucn.disc.dsm.base", appContext.getPackageName());
+    }
+}
---
+++ app/src/test/java/cl/ucn/disc/dsm/base/ExampleUnitTest.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2018.  Diego Urrutia Astorga <durrutia@ucn.cl>
+ * This work is licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.
+ * http://creativecommons.org/licenses/by-nc/4.0/
+ *
+ */
+
+package cl.ucn.disc.dsm.base;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
